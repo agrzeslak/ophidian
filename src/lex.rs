e630acaafb0de44d6bc437c2fa20ec30ec915db9
Andrzej Grzeslak <agrzeslak0@gmail.com>
2025-03-21 19:27:50 +0100

    Lex: indentation and space enforcement
    Some tokens need to be separated by spaces or come at the beginning of
    the line. This is now enforced and is a lexing error if not the case.
    
    Indentation tracking via `Indent` and `Dedent` tokens as in CPython is
    now implemented. Still need to implement the error for mixed space and
    tab usage when it is ambiguous; currently it just panics.
    
    1 test is failing because I need to fix the ambiguity between a leading
    space just being there because we've partially parsed a line and there
    was a space in it, or if it's an actual `Indent`.
    
    Also need to add tests for indentation and lexing tokens that require
    spaces to separate them.

--- src/lex.rs
+++ src/lex.rs
@@ -2,21 +2,25 @@ use miette::{Diagnostic, SourceSpan};
 use nom::{
     branch::alt,
     bytes::complete::{is_a, tag, tag_no_case, take_till, take_until, take_while},
-    character::complete::{
-        alpha1, alphanumeric1, char, digit1, line_ending, not_line_ending, one_of, tab,
-    },
-    combinator::{cut, eof, map, opt, recognize, verify},
-    multi::many0,
+    character::complete::{alpha1, char, digit1, line_ending, newline, one_of, space0, space1},
+    combinator::{cut, eof, map, opt, peek, recognize, verify},
+    multi::{fold_many0, many0},
     sequence::{delimited, pair, preceded, tuple},
     IResult,
 };
+use std::collections::VecDeque;
 use thiserror::Error;
+
+static COLUMN_WIDTH: u8 = 8;
 
 #[derive(Clone, Debug)]
 pub struct Lexer<'a> {
     whole: &'a str,
     rest: &'a str,
     position: usize,
+    indent_stack: Vec<IndentationLevel>,
+    new_line: bool,
+    token_buffer: VecDeque<Token<'a>>,
 }
 
 impl<'a> Lexer<'a> {
@@ -25,6 +29,9 @@ impl<'a> Lexer<'a> {
             whole: code,
             rest: code,
             position: 0,
+            indent_stack: Vec::new(),
+            new_line: true,
+            token_buffer: VecDeque::new(),
         }
     }
 
@@ -133,7 +140,7 @@ impl<'a> Lexer<'a> {
 
     fn parse_comment(input: &str) -> IResult<&str, ()> {
         // TODO: Parse triple-quoted docstrings
-        map(tuple((char('#'), not_line_ending)), |_| ())(input)
+        map(tuple((space0, char('#'), take_till(|c| c == '\n'))), |_| ())(input)
     }
 
     fn parse_identifier(input: &str) -> IResult<&str, Token> {
@@ -145,61 +152,151 @@ impl<'a> Lexer<'a> {
             |s| Token::Identifier(s),
         )(input)
     }
+
+    // We are following the rules used by the CPython lexer here. In summary:
+    //
+    // 1. A tab advances the cursor up to the next 8 column boundary.
+    // 2. The line is more indented if the column that the spaces and tabs result in is
+    //    greater than the current indentation level.
+    // 3. If the column value is the same as one already on the stack, but the number of
+    //    spaces and tabs differs, this is a error.
+    fn parse_indent(input: &str) -> IResult<&str, IndentationLevel> {
+        fold_many0(
+            one_of(" \t"),
+            || IndentationLevel::default(),
+            |mut acc, item| {
+                match item {
+                    ' ' => {
+                        acc.spaces += 1;
+                        acc.column += 1;
+                    }
+                    '\t' => {
+                        acc.tabs += 1;
+                        // https://github.com/python/cpython/blob/ce79274e9f093bd06d2285c9af48dbcbc92173de/Parser/lexer/lexer.c#L430
+                        acc.column = (acc.column / COLUMN_WIDTH + 1) * COLUMN_WIDTH;
+                    }
+                    _ => unreachable!(),
+                }
+                acc
+            },
+        )(input)
+    }
+
+    fn parse_empty_line(input: &str) -> IResult<&str, ()> {
+        map(pair(space0, peek(newline)), |_| ())(input)
+    }
 }
 
 impl<'a> Iterator for Lexer<'a> {
     type Item = Result<Token<'a>, LexError>;
 
     fn next(&mut self) -> Option<Self::Item> {
+        // TODO: Ignore newline if there are pending unclosed brackets or line ended with '\'
+        // TODO: When encountering '\', the indentation we have seen until it is the indentation for
+        //       the next line
+        // TODO: Tests for indentation
+        dbg!(rest);
+        if self.token_buffer.len() > 0 {
+            return Some(Ok(self.token_buffer.pop_front().expect("cannot be empty")));
+        }
+
         if self.rest.len() == 0 {
             return None;
         }
 
-        // Skip over comments
-        let (rest, _) = opt(Self::parse_comment)(self.rest).expect("does not need to match");
+        let (rest, _) = opt(Self::parse_empty_line)(self.rest).expect("does not need to match");
+        let (mut rest, _) = opt(Self::parse_comment)(rest).expect("does not need to match");
 
-        // `alt` only supports up to 21 tuple values, hence they have been split up
+        // Parse indentation. This is only done if we are on a new line, otherwise whitespace when
+        // parsing partially through a line will be treated as indentation.
+        if self.new_line {
+            let (new_rest, current_indent) =
+                Self::parse_indent(rest).expect("does not need to match");
+            rest = new_rest;
+
+            if current_indent.column == 0 {
+                // No indentation
+                for _ in 0..self.indent_stack.len() {
+                    self.token_buffer.push_back(Token::Dedent);
+                }
+            } else if self.indent_stack.len() == 0
+                || self.indent_stack.last().expect("cannot be empty").column < current_indent.column
+            {
+                // Deeper indentation
+                self.indent_stack.push(current_indent);
+                return Some(Ok(Token::Indent));
+            } else {
+                // Shallower indentation
+                while let Some(indent) = self.indent_stack.pop() {
+                    if indent.column == current_indent.column {
+                        // The indentation results in the same column, but with a different mix of
+                        // tabs and spaces
+                        if indent != current_indent {
+                            todo!("throw mixed tab/space indentation error");
+                        }
+                        break;
+                    }
+                    self.token_buffer.push_back(Token::Dedent);
+                }
+            }
+
+            // Emit the final generated `Indent`/`Dedent` token, or fall through if none were
+            // generated (same indentation)
+            if let Some(token) = self.token_buffer.pop_front() {
+                return Some(Ok(token));
+            }
+        }
+
+        // Some tokens need to be separated by whitespace, or be at the start of a new line.
+        let preceding_space_parser = if self.new_line { space0 } else { space1 };
+
         let result: nom::IResult<&str, Token, nom::error::Error<&str>> = alt((
             Self::parse_number,
             Self::parse_string,
             // Keyword
-            alt((
-                map(tag("and"), |_| Token::And),
-                map(tag("as"), |_| Token::As),
-                map(tag("assert"), |_| Token::Assert),
-                map(tag("break"), |_| Token::Break),
-                map(tag("class"), |_| Token::Class),
-                map(tag("continue"), |_| Token::Continue),
-                map(tag("def"), |_| Token::Def),
-                map(tag("del"), |_| Token::Del),
-                map(tag("elif"), |_| Token::Elif),
-                map(tag("else"), |_| Token::Else),
-                map(tag("except"), |_| Token::Except),
-                map(tag("false"), |_| Token::False),
-                map(tag("finally"), |_| Token::Finally),
-                map(tag("for"), |_| Token::For),
-                map(tag("from"), |_| Token::From),
-                map(tag("global"), |_| Token::Global),
-                map(tag("if"), |_| Token::If),
-            )),
-            alt((
-                map(tag("import"), |_| Token::Import),
-                map(tag("in"), |_| Token::In),
-                map(tag("is"), |_| Token::Is),
-                map(tag("lambda"), |_| Token::Lambda),
-                map(tag("None"), |_| Token::None),
-                map(tag("nonlocal"), |_| Token::Nonlocal),
-                map(tag("not"), |_| Token::Not),
-                map(tag("or"), |_| Token::Or),
-                map(tag("pass"), |_| Token::Pass),
-                map(tag("raise"), |_| Token::Raise),
-                map(tag("return"), |_| Token::Return),
-                map(tag("True"), |_| Token::True),
-                map(tag("try"), |_| Token::Try),
-                map(tag("while"), |_| Token::While),
-                map(tag("with"), |_| Token::With),
-                map(tag("yield"), |_| Token::Yield),
-            )),
+            preceded(
+                preceding_space_parser,
+                // `alt` only supports up to 21 tuple values, hence they have been split up
+                alt((
+                    alt((
+                        map(tag("and"), |_| Token::And),
+                        map(tag("as"), |_| Token::As),
+                        map(tag("assert"), |_| Token::Assert),
+                        map(tag("break"), |_| Token::Break),
+                        map(tag("class"), |_| Token::Class),
+                        map(tag("continue"), |_| Token::Continue),
+                        map(tag("def"), |_| Token::Def),
+                        map(tag("del"), |_| Token::Del),
+                        map(tag("elif"), |_| Token::Elif),
+                        map(tag("else"), |_| Token::Else),
+                        map(tag("except"), |_| Token::Except),
+                        map(tag("false"), |_| Token::False),
+                        map(tag("finally"), |_| Token::Finally),
+                        map(tag("for"), |_| Token::For),
+                        map(tag("from"), |_| Token::From),
+                        map(tag("global"), |_| Token::Global),
+                        map(tag("if"), |_| Token::If),
+                    )),
+                    alt((
+                        map(tag("import"), |_| Token::Import),
+                        map(tag("in"), |_| Token::In),
+                        map(tag("is"), |_| Token::Is),
+                        map(tag("lambda"), |_| Token::Lambda),
+                        map(tag("None"), |_| Token::None),
+                        map(tag("nonlocal"), |_| Token::Nonlocal),
+                        map(tag("not"), |_| Token::Not),
+                        map(tag("or"), |_| Token::Or),
+                        map(tag("pass"), |_| Token::Pass),
+                        map(tag("raise"), |_| Token::Raise),
+                        map(tag("return"), |_| Token::Return),
+                        map(tag("True"), |_| Token::True),
+                        map(tag("try"), |_| Token::Try),
+                        map(tag("while"), |_| Token::While),
+                        map(tag("with"), |_| Token::With),
+                        map(tag("yield"), |_| Token::Yield),
+                    )),
+                )),
+            ),
             // Triple char
             alt((
                 map(tag(">>="), |_| Token::DoubleGreaterThanEquals),
@@ -249,18 +346,17 @@ impl<'a> Iterator for Lexer<'a> {
                 map(char(']'), |_| Token::RightBracket),
                 map(char(')'), |_| Token::RightParen),
                 map(char('/'), |_| Token::Slash),
-                map(char(' '), |_| Token::Space),
                 map(char('*'), |_| Token::Star),
-                map(tab, |_| Token::Tab),
                 map(char('~'), |_| Token::Tilde),
             )),
-            Self::parse_identifier,
+            preceded(preceding_space_parser, Self::parse_identifier),
         ))(rest);
         match result {
             Ok((rest, token)) => {
                 // Update position based on how much the length of `rest` decreased
                 self.position += self.rest.len() - rest.len();
                 self.rest = rest;
+                self.new_line = token == Token::NewLine;
                 Some(Ok(token))
             }
             Err(_) => {
@@ -283,7 +379,7 @@ impl<'a> Iterator for Lexer<'a> {
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Token<'a> {
     Ampersand,
     AmpersandEquals,
@@ -302,6 +398,7 @@ pub enum Token<'a> {
     Comma,
     Complex(&'a str),
     Continue,
+    Dedent,
     Def,
     Del,
     Dot,
@@ -327,6 +424,7 @@ pub enum Token<'a> {
     GreaterThan,
     Identifier(&'a str),
     If,
+    Indent,
     Import,
     In,
     Int(&'a str),
@@ -355,17 +453,28 @@ pub enum Token<'a> {
     RightParen,
     Slash,
     SlashEquals,
-    Space,
     Star,
     StarEquals,
     String(&'a str),
-    Tab,
     Tilde,
     True,
     Try,
     While,
     With,
     Yield,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum IndentChar {
+    Space,
+    Tab,
+}
+
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+struct IndentationLevel {
+    pub(crate) column: u8,
+    pub(crate) spaces: u8,
+    pub(crate) tabs: u8,
 }
 
 #[derive(Debug, Diagnostic, Error)]
@@ -426,7 +535,7 @@ mod tests {
     fn ensure_taking_most_chars_possible() {
         use Token::*;
         assert_tokens_eq!(
-            r"***==*None\/",
+            r"***==* None\/",
             [DoubleStar, StarEquals, Equals, Star, None, BackSlash, Slash]
         );
     }
@@ -551,8 +660,6 @@ continue
                 NewLine,
                 NewLine,
                 String("'foo'"),
-                Space,
-                Space,
                 NewLine,
                 Continue,
                 NewLine,
@@ -567,28 +674,18 @@ continue
     #[test]
     fn identifiers() {
         use Token::*;
-        assert_tokens_eq!(
-            "a = b",
-            [Identifier("a"), Space, Equals, Space, Identifier("b")]
-        );
-        assert_tokens_eq!(" A='foo'", [Space, Identifier("A"), Equals, String("'foo'")]);
-        assert_tokens_eq!(
-            "AbCd1_23 = 2",
-            [Identifier("AbCd1_23"), Space, Equals, Space, Int("2")]
-        );
+        assert_tokens_eq!("a = b", [Identifier("a"), Equals, Identifier("b")]);
+        assert_tokens_eq!(" A='foo'", [Identifier("A"), Equals, String("'foo'")]);
+        assert_tokens_eq!("AbCd1_23 = 2", [Identifier("AbCd1_23"), Equals, Int("2")]);
         assert_tokens_eq!(
             "class Foo:\n\tA = 100",
             [
                 Class,
-                Space,
                 Identifier("Foo"),
                 Colon,
                 NewLine,
-                Tab,
                 Identifier("A"),
-                Space,
                 Equals,
-                Space,
                 Int("100")
             ]
         );
@@ -596,15 +693,11 @@ continue
             "class Foo:\n\tA = 100",
             [
                 Class,
-                Space,
                 Identifier("Foo"),
                 Colon,
                 NewLine,
-                Tab,
                 Identifier("A"),
-                Space,
                 Equals,
-                Space,
                 Int("100")
             ]
         );
